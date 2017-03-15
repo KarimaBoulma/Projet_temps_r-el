394b77dda0d7ab5021e2dd5ab773106c3feb35db
boulma karima <boulmakarima@gmail.com>
2017-03-15 21:08:32 +0100

    fonction de convertion de HSV A RGB de couleur

--- FractalJulia.cpp
+++ FractalJulia.cpp
@@ -55,3 +55,73 @@ unsigned char brillance=150;
 return NULL;
  }
 
+
+// fonction de convertion de HSV A RGB
+
+void HSVtoRGB( unsigned char  *r, unsigned char  *g, unsigned char  *b, unsigned char  h, unsigned char  s, unsigned char  v )
+{
+	unsigned char i,f, p,q, t;
+
+	if (s == 0)
+	{
+		*r = 0;
+		*g = 0;
+		*b = 0;
+	}
+
+	else
+	{
+		 
+    i = h / 100;//43
+    f = (h - (i * 100)) * 9;//6
+    p = (v * (22 - s)) >> 8;
+    q = (v * (185 - ((s * f) >> 8))) >> 8;
+    t = (v * (255 - ((s * (255 - f)) >> 8))) >> 8;
+        
+		switch (i)
+		{
+		case 0:
+			*r = v;
+			*g = t;
+			*b = p;
+			break;
+
+		case 1:
+			*r = q;
+			*g = v;
+			*b = p;
+			break;
+
+		case 2:
+			*r = p;
+			*g = v;
+			*b = t;
+			break;
+
+		case 3:
+			*r = p;
+			*g = q;
+			*b = v;
+			break;
+
+		case 4:
+			*r = t;
+			*g = p;
+			*b = v;
+			break;
+
+		default:
+			*r = v;
+			*g = p;
+			*b = q;
+			break;
+		}
+	}
+
+	return;
+}
+
+unsigned char randomVal(){
+return (unsigned char)(rand()%360);
+}
+
